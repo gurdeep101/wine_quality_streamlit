cac5184382933d77c56d9e867a09a9b9309179d1
Gurdeep <gurdeep@Gurdeeps-MacBook-Air.local>
2020-12-27 14:46:08 +0530

    sync

---
+++ code/wine_classifier_model.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 26 19:51:04 2020
+
+@author: gurdeep
+"""
+# library
+import pandas as pd
+import numpy as np
+
+wine = pd.read_csv('../data/winequality-red.csv')
+
